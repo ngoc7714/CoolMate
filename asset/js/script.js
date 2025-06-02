db9d9b9d977ada8746a41f368d6782ada6418fbb
ngoc7714 <nnngoc.20it6@vku.udn.vn>
2025-06-03 01:24:28 +0700

    v4

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,27 +1,39 @@
-const sliderItem = document.querySelectorAll('.slider-item')
+const sliderItem = document.querySelectorAll(".slider-item");
 for (let index = 0; index < sliderItem.length; index++) {
-    sliderItem[index].style.left = index * 100 + "%"
+  sliderItem[index].style.left = index * 100 + "%";
 }
 
-const sliderItems =document.querySelector('.slider-items')
+const sliderItems = document.querySelector(".slider-items");
 
-const arrowRight = document.querySelector('.ri-arrow-right-line')
-const arrowLeft = document.querySelector('.ri-arrow-left-line')
-let i = 0
-arrowRight.addEventListener('click',()=> {
-    i++
-    if(i< sliderItem.length) {
-    sliderItems.style.left = -i*100+"%"
-    }else {
-        return false
-    }
-})
-arrowLeft.addEventListener('click',()=> {
-    if(i<=0) {
-        return false
-    }{
-    i--
-    sliderItems.style.left = -i*100+"%"
-    }
-    
-})
+const arrowRight = document.querySelector(".ri-arrow-right-line");
+const arrowLeft = document.querySelector(".ri-arrow-left-line");
+let i = 0;
+arrowRight.addEventListener("click", () => {
+  if (i < sliderItem.length - 1) {
+    i++;
+    silderMove(i);
+  } else {
+    return false;
+  }
+});
+arrowLeft.addEventListener("click", () => {
+  if (i <= 0) {
+    return false;
+  } else {
+    i--;
+    silderMove(i);
+  }
+});
+function autoSlider() {
+  if (i < sliderItem.length - 1) {
+    i++;
+    silderMove(i);
+  } else {
+    i = 0;
+    silderMove(i);
+  }
+}
+function silderMove(i) {
+  sliderItems.style.left = -i * 100 + "%";
+}
+setInterval(autoSlider, 3000);
